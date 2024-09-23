6c04bcaa678c2c4cba35d55176bb11e43751b76e
TuanNCA3 <tuan.nca@anlab.biz>
2024-09-23 17:12:30 +0700

    Update augmentation.py

--- similarity matching/dataset/augmentation.py
+++ similarity matching/dataset/augmentation.py
@@ -5,7 +5,7 @@ import os
 
 # Rotation and Flipping
 def rotate(image):
-    rotated_images = [image.rotate(angle) for angle in [10, -10]]
+    rotated_images = [image.rotate(angle) for angle in [5, -5]]
     return rotated_images
 
 # Noise Injection (Gaussian Noise)
@@ -28,35 +28,37 @@ def adjust_colors(image):
     # Random adjustments
     brightness_img = enhancers[0].enhance(random.uniform(0.7, 1.3))  # Brightness range
     contrast_img = enhancers[1].enhance(random.uniform(0.7, 1.3))    # Contrast range
-    saturation_img = enhancers[2].enhance(random.uniform(0.7, 1.3))  # Saturation range
 
     return [brightness_img, contrast_img]
 
 
 if __name__ == "__main__":
 
-    root = "F:/label_data_final"
+    root = ""
 
     sets = os.listdir(root)
 
     for set_ in sets: 
         imgs = os.listdir(f"{root}/{set_}")
-
+        s_ = 0
         for img in imgs: 
-            img_name = img.split(".")[0]
             pimg = Image.open(f"{root}/{set_}/{img}")
-
+            
             rotated_imgs = rotate(pimg)
             noisy_img = add_gaussian_noise(pimg)
             color_adjusted_imgs = adjust_colors(pimg)
 
+            img_name = set_
+
             for i, rotated_img in enumerate(rotated_imgs): 
-                rotated_img.save(f"{root}/{set_}/{img_name}_{i+1}.jpg")
+                rotated_img.save(f"{root}/{set_}/{img_name}_m{s_}{i+1}.jpg")
 
-            for i, color_adjusted_img in enumerate(rotated_imgs): 
-                rotated_img.save(f"{root}/{set_}/{img_name}_{i+3}.jpg")
+            for i, color_adjusted_img in enumerate(color_adjusted_imgs): 
+                rotated_img.save(f"{root}/{set_}/{img_name}_m{s_}{i+3}.jpg")
 
-            noisy_img.save(f"{root}/{set_}/{img_name}_5.jpg")
+            noisy_img.save(f"{root}/{set_}/{img_name}_m{s_}5.jpg")
+
+            s_ += 1
 
 
 
