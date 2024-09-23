a1fff03221b76f9a8d97e5570c2cd7f486697fcd
TuanNCA3 <tuan.nca@anlab.biz>
2024-09-23 16:33:13 +0700

    Update constant.py

--- similarity matching/dataset/constant.py
+++ similarity matching/dataset/constant.py
@@ -1,8 +1,7 @@
+import os 
+root = "" # Put your data root here
 
+# LABEL2ID = {value: key for key, value in enumerate(os.listdir(root))}
+# print(LABEL2ID)
 
-
-ID2LABEL = {"0": "Activision", "1": "AFKArena", "2": "AppleStore", "3": "Arknights", "4": "AzurLane", "5": "CallofDutyMobileVN", "6": "CallofDutyWarzoneMobile", "7": "GooglePlay", "8": "HarryPotterPuzzles&Spells", "9": "IdleHeroes", "10": "MafiaCity", "11": "MobileLegendsAdventure", "12": "MobileLegendsBangBang", "13": "MythicSummonIdleRPG", "14": "Nintendodistributor\u010cesk\u00e1aSlovensk\u00e1republika", "15": "RagnarokXNextGeneration", "16": "RaidShadowLegends", "17": "RiseofKingdoms", "18": "SevenKnights2", "19": "TheSevenDeadlySinsIdleAdventure", "20": "VNGGames", "21": "WarandMagic", "22": "WestGame", "23": "WWESuperCard"}
-
-
-LABEL2ID = {'Activision': 0, 'AFKArena': 1, 'AppleStore': 2, 'Arknights': 3, 'AzurLane': 4, 'CallofDutyMobileVN': 5, 'CallofDutyWarzoneMobile': 6, 'GooglePlay': 7, 'HarryPotterPuzzles&Spells': 8, 'IdleHeroes': 9, 'MafiaCity': 10, 'MobileLegendsAdventure': 11, 'MobileLegendsBangBang': 12, 'MythicSummonIdleRPG': 13, 'Nintendodistributor\u010cesk\u00e1aSlovensk\u00e1republika': 14, 'RagnarokXNextGeneration': 15, 'RaidShadowLegends': 16, 'RiseofKingdoms': 17, 'SevenKnights2': 18, 'TheSevenDeadlySinsIdleAdventure': 19, 'VNGGames': 20, 'WarandMagic': 21, 'WestGame': 22, 'WWESuperCard': 23}
-print({value: int(key) for key, value in ID2LABEL.items()})
+LABEL2ID = {0: "lb1", 1: "lb2"}
