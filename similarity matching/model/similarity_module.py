e62606431435c558795352915d94a3efb7bf5465
TuanNCA3 <tuan.nca@anlab.biz>
2024-10-18 10:28:19 +0700

    Update similarity_module.py

--- similarity matching/model/similarity_module.py
+++ similarity matching/model/similarity_module.py
@@ -2,29 +2,6 @@ import torch
 import torch.nn as nn 
 from torchvision import models
 from torchvision.models.resnet import ResNet50_Weights, ResNet18_Weights
-
-
-class ProjectionHead(nn.Module): 
-    def __init__(self, input_size, output_size): 
-        super(ProjectionHead, self).__init__()
-        self.input_size  = input_size 
-        self.output_size = output_size
-
-    # Architecture 
-        self.Head = nn.Sequential( 
-            nn.Linear(input_size, 1024), 
-            nn.BatchNorm1d(1024),
-            nn.GELU(),
-
-            nn.Linear(1024, 512), 
-            nn.BatchNorm1d(512),
-            nn.GELU(),
-            
-            nn.Linear(512, self.output_size)
-        )
-
-    def forward(self, X): 
-        return self.Head(X)
         
 class ContrastiveEmbedding(nn.Module): 
     def __init__(self, embedding_size=128, req_grad=False): 
